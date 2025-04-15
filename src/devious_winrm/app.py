04e9b1f47efcd6bdb3f1b3e1923082152ebeb82c
Pablo Comino <55856800+probablypablito@users.noreply.github.com>
2025-04-15 16:23:53 -0400

    Wrap commands with Out-String
    This results in a massive performance increase because PowerShell sucks at sending arrays

--- src/devious_winrm/app.py
+++ src/devious_winrm/app.py
@@ -9,37 +9,46 @@ class Terminal:
         ''' Initialize the terminal with a connection object.'''
         self.session = PromptSession()
         self.conn = conn
+        self.error_count: int = 0
 
     def run(self):
         with psrp.SyncRunspacePool(self.conn) as rp:
-            ps = psrp.SyncPowerShell(rp)
-            error_count: int = 0
+            self.ps = psrp.SyncPowerShell(rp)
             while True:
                 try:
-                    # Get current directory
-                    current_dir = ps.add_script('pwd').invoke()
-                    # print(f"Current Directory: {current_dir[0]}")
+                    current_dir = self.ps.add_script('pwd').invoke()
                     user_input = self.session.prompt(str(current_dir[0]) + "> ")
-                    ps.add_script(user_input)
-                  
+                   
+                    if user_input == 'exit':
+                        raise SystemExit                  
+                    
                     try:
-                        out = ps.invoke()
-
-                        had_error: bool = len(ps.streams.error) > error_count
-
-                        if had_error:
-                            print(ps.streams.error[-1])
-                            error_count = len(ps.streams.error)
-                        print()
-                        for line in out:
-                            print(line)
+                        self.run_command(user_input)
                     except Exception as e:
                         print(e)
                     finally:
-                        ps._pipeline.metadata.commands = []
-                except (KeyboardInterrupt, EOFError):
+                        print()
+                        self.ps._pipeline.metadata.commands = []
+                except (KeyboardInterrupt, EOFError, SystemExit):
                     print('Goodbye!')
+                    self.ps.close()
                     break
+                except Exception as e:
+                    print(e)
+                    break
+    
+    def run_command(self, command: str):
+        ''' Run a command in the terminal and print the output '''
+        self.ps.add_script(command)
+        self.ps.add_command('Out-String')
+        out = self.ps.invoke()
+
+        had_error: bool = len(self.ps.streams.error) > self.error_count
+        if had_error:
+            print(str(self.ps.streams.error[-1]).strip())
+            self.error_count = len(self.ps.streams.error)
+        if len(out[0]) > 0:
+            print(out[0].strip())
 
 
 
