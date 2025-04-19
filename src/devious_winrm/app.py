364d0a5dbc7a806ec3104f226951d1ceeb6d639c
Pablo Comino <55856800+probablypablito@users.noreply.github.com>
2025-04-19 03:03:43 -0400

    Refactor connection code out of Terminal class

--- src/devious_winrm/app.py
+++ src/devious_winrm/app.py
@@ -12,62 +12,56 @@ from devious_winrm.util.commands import commands, run_command
 class Terminal:
     """Terminal class to handle the connection and command execution."""
 
-    def __init__(self, conn: WSManInfo) -> None:
+    def __init__(self, conn: WSManInfo, ps: psrp.SyncPowerShell) -> None:
         """Initialize the terminal with a connection object."""
         self.session = PromptSession()
         self.conn = conn
+        self.ps = ps
         self.error_count: int = 0
 
     def run(self) -> None:
         """Run the terminal and handle user input."""
-        with psrp.SyncRunspacePool(self.conn) as rp:
-            self.ps = psrp.SyncPowerShell(rp)
-            while True:
-                try:
-                    current_dir = self.ps.add_script("pwd").invoke()
-                    user_input: str = self.session.prompt(str(current_dir[0]) + "> ")
-                    if user_input.split(" ")[0] in commands:
-                        run_command(self, user_input)
-                        continue
-                    try:
-                        self.run_command(user_input)
-                    except Exception as e:
-                        print_ft(e)
-                        break
-                    finally:
-                        print_ft()
-                        # Clear the commands in the pipeline
-                        self.ps._pipeline.metadata.commands = []  # noqa: SLF001
-                except (KeyboardInterrupt, EOFError, SystemExit):
-                    print_ft("Goodbye!")
-                    self.ps.close()
-                    break
-                except Exception as e:
-                    print_ft(e)
-                    break
+        while True:
+            current_dir = self.ps.add_script("pwd").invoke()
+            user_input: str = self.session.prompt(str(current_dir[0]) + "> ")
+            self.process_command(user_input)
 
-    def run_command(self, command: str) -> None:
+    def process_command(self, user_input: str) -> None:
         """Run a command in the terminal and print_ft the output."""
-        self.ps.add_script(command)
-        self.ps.add_command("Out-String")
-        out = self.ps.invoke()
+        command: str = user_input.split(" ")[0]
+        if command in commands:
+            run_command(self, user_input)
+            return
+        try:
+            self.ps.add_script(user_input)
+            self.ps.add_command("Out-String")
+            out: str = self.ps.invoke()[0]
 
-        had_error: bool = len(self.ps.streams.error) > self.error_count
-        if had_error:
-            print_ft(str(self.ps.streams.error[-1]).strip())
-            self.error_count = len(self.ps.streams.error)
-        if len(out[0]) > 0:
-            print_ft(out[0].strip())
+            # The pipeline only keeps track of total erros, not per command
+            # So we need to check if the error stream has changed
+            had_error: bool = len(self.ps.streams.error) > self.error_count
+            if had_error:
+                print_ft(str(self.ps.streams.error[-1]).strip())
+                self.error_count = len(self.ps.streams.error)
+
+            if out:
+                print_ft(out.strip())
+        except Exception as e:
+            print_ft(e)
+            return
+        finally:
+            print_ft()
+            # Clear the commands in the pipeline
+            self.ps._pipeline.metadata.commands = []  # noqa: SLF001
+
 
 
 def main(conn: WSManInfo) -> None:
     """Run the terminal application."""
-    terminal = Terminal(conn)
-    try:
+    with psrp.SyncRunspacePool(conn) as rp:
+        ps = psrp.SyncPowerShell(rp)
+        terminal = Terminal(conn, ps)
         terminal.run()
-    except Exception as e:
-        print_ft(e)
-        typer.Exit()
 
 if __name__ == "__main__":
     typer.run(main)
