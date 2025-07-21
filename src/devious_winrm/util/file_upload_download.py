2a48e2ceb321fe53b2ac5e37a70cb4acf202780e
Pablo Comino <pablo@pablocomino.com>
2025-07-21 02:03:48 -0400

    Add HTTP server for uploading files

---
+++ src/devious_winrm/util/file_upload_download.py
@@ -0,0 +1,85 @@
+"""Handle uploading and downloading files using HTTP."""
+
+import errno
+import http.server
+import socket
+from functools import partial
+from pathlib import Path
+from random import randint
+from socketserver import TCPServer
+
+LOWER_PORT_BOUND = 1024
+UPPER_PORT_BOUND = 65535
+MAX_ATTEMPTS = 3
+"""Number of attempts to try uploading before failing."""
+
+class SingleFileGETHandler(http.server.SimpleHTTPRequestHandler):
+    """Serve a single file."""
+
+    def __init__(self, file: bytes, file_size: int,
+                 request: socket.socket,
+                 client_address: tuple,
+                 server: TCPServer) -> None:
+        """Set the file to be served."""
+        self.file = file
+        self.file_size = str(file_size)
+        super().__init__(request, client_address, server)
+
+    def do_GET(self) -> None:  # noqa: N802
+        """Handle GET requests by serving the predefined HTML_CONTENT."""
+        self.send_response(200)
+        self.send_header("Content-type", "application/octet-stream")
+        self.send_header("Content-Length", self.file_size)
+        self.end_headers()
+
+        self.wfile.write(self.file)
+
+class MaxAttemptsExceededError(Exception):
+    """Exception for too many attempts."""
+
+def run_server(file: Path, file_size: int, port: int) -> None:
+    """Run the HTTP server.
+
+    The server will stop after handling the first GET request.
+    """
+    GETHandler = partial(SingleFileGETHandler, file, file_size)  # noqa: N806
+    with TCPServer(("", port),
+                    RequestHandlerClass=GETHandler,
+                    bind_and_activate=False) as tcp_socket:
+        tcp_socket.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        tcp_socket.server_bind()
+        tcp_socket.server_activate()
+        tcp_socket.handle_request()
+
+
+
+
+def upload_file(file_path: Path) -> None:
+    """Upload a file to the remote server via HTTP.
+
+    Returns:
+        str: Path on the remote host where the file was uploaded.
+
+    """
+    attempts = 0
+    while attempts < MAX_ATTEMPTS:
+        port = randint(LOWER_PORT_BOUND, UPPER_PORT_BOUND)  # noqa: S311
+        file_path = file_path.expanduser().resolve()
+        try:
+            with file_path.open(mode="rb") as file:
+                file_contents = file.read()
+                size = file.tell()
+                run_server(file_contents, size, port)
+        except OSError as e:
+            # In case port is already being used, try again
+            if e.errno == errno.EADDRINUSE:
+                attempts += 1
+                continue
+            raise
+        break
+
+    error = f"Too many attempts failed when uploading {file_path}"
+    raise MaxAttemptsExceededError(error)
+
+if __name__ == "__main__":
+    upload_file(Path("~/test/1GB.file"))
