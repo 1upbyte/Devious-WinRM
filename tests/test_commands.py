f3d19cb70f1faaa9ccdee58a6c66ffeba1016ea5
Pablo Comino <pablo@pablocomino.com>
2025-07-28 01:52:56 -0400

    Add a note about writing performant tests

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,14 +1,27 @@
-"""Test various commands."""
+"""Test various commands.
+
+Note: It is important to try to use commands that do not return Powershell arrays\
+    wherever possible. Sending arrays through Powershell is slow and should only\
+    be done when necessary.
+
+    The two most common solutions are to:
+        - Use a native binary / cmd.exe command (more vs. cat, curl.exe vs curl)
+        - Append "| Out-String" to the command. This may change the output, though.\
+          For example "pwd" no longer returns just the directory.
+"""
+from typing import TYPE_CHECKING
 
 import pytest
-from psrp import SyncRunspacePool, WSManInfo
 
 from devious_winrm.util.get_command_output import get_command_output
 
+if TYPE_CHECKING:
+    from psrp import SyncRunspacePool
+
 
 @pytest.mark.requires_server
-def test_file(connection: WSManInfo) -> None:
+def test_file(rp: "SyncRunspacePool") -> None:
     """Test reading a text file."""
-    with SyncRunspacePool(connection) as rp:
-        assert get_command_output(rp, "cat C:\\DWRM-TEST\\text.txt | Out-String")[0].strip() == "Test File"
+    cmd = "more C:\\DWRM-TEST\\text.txt"
+    assert get_command_output(rp, cmd)[0].strip() == "Test File"
 
